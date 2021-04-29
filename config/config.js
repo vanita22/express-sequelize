dc41acce66a85fa64ee35d2675fa7f4438789383
vanita22 <inesvanessapcm@gmail.com>
2021-04-29 00:23:18 -0400

    cambios

--- config/config.js
+++ config/config.js
@@ -16,11 +16,7 @@ module.exports ={
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres",
+    use_env_variable: 'DATABASE_URL',
     dialectOptions: {
       ssl: {
         require: true,
