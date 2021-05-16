4951de1bf48fd13b803c3210912b33fcef7684af
vanita22 <inesvanessapcm@gmail.com>
2021-05-16 01:25:52 -0400

    subiendo cambios

--- index.js
+++ index.js
@@ -15,21 +15,22 @@ app.use(express.urlencoded({extended: false}));
 //read
 app.get("/account_types", async (req,res) => {
     let result = await accountTypes.findAll({include: [{model: accounts}]});
-    res.send(console.log(JSON.stringify(result.map(accountT => accountT.get({plain: true})))));
-    //res.render("account_types", {accountTypes: result});
+    //res.send(console.log(JSON.stringify(result.map(accountT => accountT.get({plain: true})))));
+    res.render("account_types", {accountTypes: result});
 });
 
 app.get("/account", async (req,res) => {
-    let result = await accounts.findAll({include: [{model: accountTypes}, {model: clients}, {model: transactions}]});
-    res.send(JSON.stringify(result));
-    //res.render("account", {account: result});
+    let result = await accounts.findAll({include: [{model: accountTypes}, {model: clients}, {model: transactions}], raw: true, nest: true});
+    console.log(result);
+    //res.send(JSON.stringify(result));
+    res.render("account", {account: result});
 });
 
 app.get("/clients", async (req,res) => {
     let result = await clients.findAll({include: [{model: accounts}]});
     //let result = await account.findAll({raw: true, include: ["clients"], nest: true});
     console.log(JSON.stringify(result.map(client => client.get({plain: true}))));
-    res.render("clients");
+    res.render("clients", {clients: result});
 });
 
 app.get("/transactions", async (req,res) => {
@@ -57,12 +58,41 @@ app.post("/account_types", async (req, res) => {
         console.log(req.body);
         let result = await accountTypes.create({name, description});
         console.log(result);
-        res.send("se ha agregado un tipo de cuenta");
+        console.log(res.send("se ha agregado un tipo de cuenta"));
     } catch(error) {
         console.log(error);
         res.status(400).send("no se ha podido agregar");
     }    
 });
+
+//update
+app.put("/clients", async (req, res) => {
+    const {first_name, last_name, email, telephone, create_at, update_at} = req.body
+    try{
+        console.log(req.body);
+        let result = await clients.update(first_name, last_name, email, telephone);
+        console.log(result);
+        res.send("se ha actualizado el cliente");
+    } catch(error) {
+        console.log(error);
+        res.status(400).send("no se ha podido agregar");
+    }    
+});
+
+//delete
+app.delete("/clients", async (req, res) => {
+    const {first_name, last_name, email, telephone, create_at, update_at} = req.body
+    try{
+        console.log(req.body);
+        let result = await clients.delete(req.body);
+        console.log(result);
+        res.send("se ha eliminado el cliente");
+    } catch(error) {
+        console.log(error);
+        res.status(400).send("no se ha podido agregar");
+    }    
+});
+
 
 
 const PORT = process.env.PORT || 8080;
